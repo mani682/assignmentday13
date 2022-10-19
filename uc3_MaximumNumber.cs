2d3c52cc7d712d252de2fd0e84b146ce10cbdfc7
mani682 <manichandanamiriyala682@gmail.com>
2022-10-19 09:20:05 +0530

    maximum string

---
+++ uc3_MaximumNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTestMax
+{
+    public class uc3_MaximumNumber
+    {
+        public string MaximumIntegerNumber(string firstNumber, string secondNumber, string thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            else if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            else if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            else
+            {
+
+                return null;
+            }
+        }
+    }
+}
