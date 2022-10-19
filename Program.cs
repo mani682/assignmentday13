db1106451881c2c76a17a01e73da18be6262eb3a
mani682 <manichandanamiriyala682@gmail.com>
2022-10-19 09:13:19 +0530

    generic test maximum

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericFindMax
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FindMaxNo findMaxNo = new FindMaxNo();
+            findMaxNo.MaximumNum(100, 200, 300);
+
+            MaximumNumber maximumNumber = new MaximumNumber();
+            maximumNumber.MaximumNumber("vikas", "vikaaas", "vikaaaaaaas");
+
+            FindMaxFloat findMaxfloat = new FindMaxFloat();
+            findMaxfloat.MaximumFloat(8.1, 8.8, 7.6);
+        }
+    }
+}
